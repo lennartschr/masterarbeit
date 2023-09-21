9d7cefc4d45bf9af7d7d750bca84e4202a676a3c
lennart.schroeder1@uni-goettingen.de <lennartschroeder@MBP-von-Lennart.fritz.box>
2023-09-21 22:43:37 +0200

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn prototyp.wsgi:apllication --log-file -
--- prototyp/settings.py
+++ prototyp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
+import django_heroku
+import dj_database_url
 import os # HINZUFÜGEN!!!
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # HINZUFÜGEN!!!
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+django_heroku.settings(locals())
